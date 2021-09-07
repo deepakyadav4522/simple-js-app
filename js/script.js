76c993a51e780978aaf01c72d1f5e0db9cedb7a3
deepakyadav <dy9876907@gmail.com>
2021-09-07 11:37:33 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-alert('Hello World')
+var pokemonList = [
+    {name: 'Bulbasaur', height: 7, type: [grass, poison]},
+    {name: 'Pikachu', height: 8, type: [Electic, speed]},
+    {name: 'Charizard', height: 15, type: [fire, flying]}
+];
