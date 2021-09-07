763dd035050480e02d3326428476521672c38ea7
deepakyadav <dy9876907@gmail.com>
2021-09-07 12:38:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-var pokemonList = [
-    {name: 'Bulbasaur', height: 7, type: [grass, poison]},
-    {name: 'Pikachu', height: 8, type: [Electic, speed]},
-    {name: 'Charizard', height: 15, type: [fire, flying]}
+const pokemonList = [
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+    {name: 'Pikachu', height: 8, type: ['Electic', 'speed']},
+    {name: 'Charizard', height: 15, type: ['fire', 'flying']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    
+
+    if(pokemonList[i].height < 10)
+    {document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meter tall." + '<br>')
+}   else
+    {document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meter tall. " + " - Wow, that's big!"+ '<br>')};
+
+}    
