3e268b9c4b6999f29c56c7127c8b534ceef5e72f
deepakyadav <dy9876907@gmail.com>
2021-09-08 00:34:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-const pokemonList = [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
-    {name: 'Pikachu', height: 8, type: ['Electic', 'speed']},
-    {name: 'Charizard', height: 15, type: ['fire', 'flying']}
-];
+let pokemonRepository = (function () {
+    const pokemonList = [
+        {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+        {name: 'Pikachu', height: 8, type: ['Electic', 'speed']},
+        {name: 'Charizard', height: 15, type: ['fire', 'flying']}
+    ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    
 
-    if(pokemonList[i].height < 10)
-    {document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meter tall." + '<br>')
-}   else
-    {document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meter tall. " + " - Wow, that's big!"+ '<br>')};
+    function getAll() {
+        return pokemonList;
+    }
 
-}    
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + " is " + pokemon.height + " meter tall <br>" ); 
+});
+  
