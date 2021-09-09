3887e436ac35b2dfa815cbab5c23218a4bddc3e1
deepakyadav <dy9876907@gmail.com>
2021-09-09 14:44:37 -0400

    First I added Pokemons and their details by myself  and I made a function in which i made a couple of elements inside an unordered list and then I added event listener. When I click the button the system logs the respective pokemon and it;s details

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let pokemonRepository = (function () {
-    const pokemonList = [
+    let pokemonList = [
         {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
         {name: 'Pikachu', height: 8, type: ['Electic', 'speed']},
         {name: 'Charizard', height: 15, type: ['fire', 'flying']}
     ];
 
+    
 
     function getAll() {
         return pokemonList;
@@ -14,13 +15,34 @@ let pokemonRepository = (function () {
         pokemonList.push(pokemon);
     }
 
+    function addListItem(pokemon) {
+        let pokemonUl = document.querySelector('.pokemon-list');
+        let listItem  = document.createElement('li');
+        listItem.classList.add('listItem');
+        let button = document.createElement('button');
+        button.classList.add('btn-class');
+        button.innerText = pokemon.name;
+        listItem.appendChild(button);
+        pokemonUl.appendChild(listItem);
+        button.addEventListener('click', function(event){
+            showDetails(pokemon);
+        });
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    };
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+
     };
 })();
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + " is " + pokemon.height + " meter tall <br>" ); 
-});
   
+pokemonRepository.add({name: "Onix", height: 20, type: ["rock", "earth"]});  
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
